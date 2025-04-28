4ae7cbd64034bc4dcbda68400deb32147a50125e
domdomTH <dnadomdom@gmail.com>
2025-04-29 01:59:39 +0700

    first commit

---
+++ github-config.js
@@ -0,0 +1,8 @@
+const GITHUB_CONFIG = {
+    username: 'domdomTH',
+    repoName: 'noarz-data',
+    token: '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',
+    enabled: true
+};
+
+window.GITHUB_CONFIG = GITHUB_CONFIG;
